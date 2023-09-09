5dba05b1222acbe6171c6c0d0e31f779d0d8b00b
Collins Okafor <69325061+collins-okafor@users.noreply.github.com>
2023-09-09 22:03:16 +0100

    updated

---
+++ api/Models/UserDetail.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class UserDetail
+    {
+        public string? Slack_name { get; set; }
+        public string? Current_day { get; set; }
+        public string? Utc_time { get; set; }
+        public string? Track { get; set; }
+        public string? Github_file_url { get; set; }
+        public string? Github_repo_url { get; set; }
+        public int? Status_code { get; set; }
+    }
+}
